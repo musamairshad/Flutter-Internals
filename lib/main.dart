1eb636989f54dfb2e66d30c6a50637b143b6b452
Muhammad Usama <imusamairshad@gmail.com>
2023-06-18 17:28:14 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_internals/ui_updates_demo.dart';
+// import 'package:flutter_internals/ui_updates_demo.dart';
+import 'package:flutter_internals/keys/keys.dart';
 
 void main() {
+  // final numbers = const [1, 2, 3];
+  // final numbers = [1, 2, 3];
+  // const numbers = [1, 2, 3];
+  // var numbers = [1, 2, 3];
+  // // numbers = [4, 5, 6];
+  // numbers.add(4);
   runApp(const MyApp());
 }
 
@@ -17,7 +24,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter Internals'),
         ),
-        body: const UIUpdatesDemo(),
+        // body: const UIUpdatesDemo(),
+        body: const Keys(),
       ),
     );
   }
